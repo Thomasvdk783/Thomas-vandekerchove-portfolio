05d072ab0014c3a874441f4a6ddfe69ac261f3a9
Thomasvdk783 <thomas.vandekerchove@outlook.com>
2022-08-07 20:56:46 +0200

    premier commit du projet protfolio avec l'utilisation de bootstrap 5

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// js for nav //
+
+const icone = document.querySelector('.navbar-mobile i');
+const modal = document.querySelector('.modal')
+
+icone.addEventListener('click', function(){
+    // console.log('Le clique fonctionne')
+    modal.classList.toggle('change-modal')
+    icone.classList.toggle('fa-times')
+});

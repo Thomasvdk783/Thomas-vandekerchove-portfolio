80265593c6d30818393fb5b6c61e908e7a89683d
Thomasvdk783 <thomas.vandekerchove@outlook.com>
2022-08-19 20:03:09 +0200

    animation titre homepage

---
+++ index.js
@@ -0,0 +1,24 @@
+const textAnim = document.querySelector('h1');
+
+new Typewriter(textAnim, {
+    loop: true,
+    deleteSpeed: 20
+})
+
+.changeDelay(100)
+.typeString("Hello ! Moi c'est Thomas")
+.pauseFor(300)
+.typeString("<strong>, Dev Frond-End</strong> !")
+.pauseFor(1000)
+.deleteChars(12)
+.typeString('<span style="color:#ff6910"> HTML</span> !')
+.pauseFor(1000)
+.deleteChars(6)
+.typeString('<span style="color:#27ea60"> CSS</span> !')
+.pauseFor(1000)
+.deleteChars(5)
+.typeString('<span style="color:midnightblue"> React</span> !')
+.pauseFor(1000)
+.deleteChars(7)
+.typeString('<span style="color:#ff6910"> JavaScript</span> !')
+.start()
